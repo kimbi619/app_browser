37c189396e936dd49433571624c3d367a0f72591
kimbi619 <kimbidarl@gmail.com>
2024-12-05 07:09:00 -0500

    feat:: made some changes

--- components/AppBrowser.js
+++ components/AppBrowser.js
@@ -1,32 +1,33 @@
 import { Button, Linking, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import * as WebBrowser from 'expo-web-browser'
+
 export default function AppBrowser() {
 
-    const openDefaultBrowser = (url) => {
-        try {
-            Linking.canOpenURL(url)
-            .then(supported => {
-                if(supported) {
-                    Linking.openURL(url)
-                } else {
-                    console.log("Don't know how to open URI: " + url)
-                }
-            })
-        } catch (error) {
-            console.warn('An error occurred', error);
-        }
-    }
-
-    const openInInApp = async(url) => {
-        let result = await WebBrowser.openBrowserAsync(url)
+    const handleOpenInApp = async() => {
+        let result = await WebBrowser.openBrowserAsync('https://www.google.com')
         console.log(result)
     }
 
+    const handleOpenInDefaultBrowser = (url) => {
+        Linking.canOpenURL(url)
+        .then((supported) => {
+            if (!supported) {
+                console.log("Can't handle url: " + url);
+            } else {
+                return Linking.openURL(url);
+            }
+        })
+        .catch((err) => {
+            console.warn("Error: ", err);
+            
+        })
+
+    }
   return (
-    <View>
-        <Button title="Open in Default Browser" onPress={() => openDefaultBrowser("http://google.com")} />
-        <Button style={styles.btn} title="Open in App" onPress={() => openInInApp("http://google.com")} />
+    <View style={{ flex: 1, width: '100%', paddingTop: 50}}>
+        <Button title="Open In App Browser" onPress={handleOpenInApp} />
+        <Button title="Open In Default Browser" onPress={() => handleOpenInDefaultBrowser('https://example.com')} />
     </View>
   )
 }
